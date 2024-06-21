b92803144162e854d1cd6a1b801681ee7ef73b85
yuichkun <yuichkun@gmail.com>
2024-06-21 16:53:50 +0900

    inherit default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		colors: {
+			...colors,
 			primary: '#BCC1D1'
 		},
 		extend: {}
