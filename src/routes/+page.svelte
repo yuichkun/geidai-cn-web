69b7c4f969695255127be26c90a6a96df5e8d09a
yuichkun <yuichkun@gmail.com>
2024-06-21 17:22:48 +0900

    swap description with image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,7 @@
 
 <div class="relative">
 	<MainImage />
-	<div class="fixed bottom-8 right-8 text-white">
-		<h1>
-			<span class="text-[7vw] lg:text-6xl"> 東京藝術大学と中国人留学生 </span>
-			<br />
-			<span class="text-[5vw] text-right block lg:text-4xl"> 〜 李叔同から現代まで 〜展 </span>
-		</h1>
+	<div class="fixed bottom-8 right-0 lg:right-8 p-8 lg:p-0 text-white w-full lg:w-[640px]">
+		<enhanced:img src="$lib/images/description.svg" alt="description" />
 	</div>
 </div>
