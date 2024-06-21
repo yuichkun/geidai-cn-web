f322e24c729d8ea91208bea16a5c10971343ea2d
yuichkun <yuichkun@gmail.com>
2024-06-21 17:00:31 +0900

    add header text to top page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
+<script>
+	import MainImage from '$lib/MainImage.svelte';
+</script>
+
 <div class="relative">
 	<div class="fixed top-8 right-8">
 		<enhanced:img class="w-6" src="$lib/images/hamburger.svg" />
 	</div>
-	<enhanced:img
-		class="w-screen h-screen object-cover lg:w-[460px] lg:h-auto"
-		src="$lib/images/main-image.png"
-		alt="李叔同"
-	/>
+	<MainImage />
+	<div class="fixed bottom-8 right-8 text-white">
+		<h1>
+			<span class="text-[7vw] lg:text-6xl"> 東京藝術大学と中国人留学生 </span>
+			<br />
+			<span class="text-[5vw] text-right block lg:text-4xl"> 〜 李叔同から現代まで 〜展 </span>
+		</h1>
+	</div>
 </div>
