3171e012673533ec380a12caadcca98578c9ab7a
yuichkun <yuichkun@gmail.com>
2024-06-21 11:27:15 +0900

    adjust main image position

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>てすと</h1>
-<enhanced:img src="$lib/images/main-image.png" alt="画像" />
+<div class="">
+	<enhanced:img
+		class="w-screen h-screen object-cover lg:w-[460px] lg:h-auto"
+		src="$lib/images/main-image.png"
+		alt="李叔同"
+	/>
+</div>
