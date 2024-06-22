d8261c77ef15a5d4e4b084c4a72512e27c11d3a2
yuichkun <yuichkun@gmail.com>
2024-06-22 16:37:25 +0900

    adjust styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
 <div class="relative">
 	<MainImage />
-	<div class="fixed bottom-8 right-0 lg:right-8 p-8 lg:p-0 text-white w-full lg:w-[640px]">
-		<enhanced:img src="$lib/images/description.svg" alt="description" />
+	<div class="fixed bottom-8 right-0 lg:right-8 p-2 lg:p-0 text-white w-full lg:w-[640px]">
+		<enhanced:img src="$lib/images/description.svg" alt="description" class="w-full" />
 	</div>
 </div>
