ab3018b585619efa7aeaa90adf3522ef9e169e16
yuichkun <yuichkun@gmail.com>
2024-07-01 07:39:32 +0900

    update everything

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
 	import '../app.css';
-	import Menu from '$lib/Menu.svelte';
 </script>
 
 <div class="relative">
-	<div class="fixed top-8 right-10 z-50">
-		<Menu />
-	</div>
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,22 @@
-<script>
-	import MainImage from '$lib/MainImage.svelte';
-</script>
-
 <div class="relative">
-	<MainImage />
-	<div class="fixed bottom-8 right-0 lg:right-8 p-2 lg:p-0 text-white w-full lg:w-[640px]">
-		<enhanced:img src="$lib/images/description.svg" alt="description" class="w-full" />
+	<!-- SP -->
+	<div class="lg:hidden">
+		<enhanced:img class="w-screen" src="$lib/images/main-image.png" alt="李叔同" />
+		<div class="flex justify-end">
+			<img class="mt-5 mr-2" src="/map.svg" alt="map" />
+		</div>
+		<img class="ml-4" src="/description.svg" alt="map" />
+	</div>
+	<!-- PC -->
+	<div class="hidden lg:flex">
+		<enhanced:img
+			class="h-screen w-auto p-8 min-w-[670px]"
+			src="$lib/images/main-image.png"
+			alt="李叔同"
+		/>
+		<div class="flex flex-col justify-between flex-grow">
+			<img class="mt-5 ml-auto w-[320px]" src="/map.svg" alt="map" />
+			<img class="mb-7 w-[680px]" src="/description.svg" alt="map" />
+		</div>
 	</div>
 </div>
